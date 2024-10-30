ad88421e9109b93f03d413fcf6326ca9bcd0e8fd
sweeppy <104524272+sweeppy@users.noreply.github.com>
2024-10-31 01:41:14 +0300

    Started making the first page
    Changing pictures to svg format

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Welcome from "./Components/Home/Welcome";
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="" element={<Welcome />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/Components/Home/Welcome.tsx
@@ -0,0 +1,18 @@
+const Welcome = () => {
+  return (
+    <main data-center>
+      <div className="even-colums">
+        <div>
+          <img
+            height={300}
+            src="/public/assets/boards/plain_01.svg"
+            alt="chess_board"
+          />
+        </div>
+        <div>text</div>
+      </div>
+    </main>
+  );
+};
+
+export default Welcome;

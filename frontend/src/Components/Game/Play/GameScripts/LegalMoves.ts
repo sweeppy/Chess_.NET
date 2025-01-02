a330582f8f9336acd701cfc32e132be8ccf22894
sweeppy <104524272+csharp-enjoyer@users.noreply.github.com>
2025-01-02 21:45:47 +0300

    Done with king's legal moves

--- frontend/src/Components/Game/Play/GameScripts/LegalMoves.ts
+++ frontend/src/Components/Game/Play/GameScripts/LegalMoves.ts
@@ -3,11 +3,12 @@ import { ChessPiece } from "./Pieces";
 const letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
 const numbers = ["8", "7", "6", "5", "4", "3", "2", "1"];
 
-const showLegalMoves = (piece: ChessPiece, alivePieces: ChessPiece[]) => {
-  const currentColumn = piece.position[0];
-  const currentRow = piece.position[1];
-  const columnIndex = letters.indexOf(currentColumn);
-  const rowIndex = numbers.indexOf(currentRow);
+const getLegalMoves = (
+  piece: ChessPiece,
+  alivePieces: ChessPiece[]
+): string[] => {
+  const columnIndex = letters.indexOf(piece.position[0]);
+  const rowIndex = numbers.indexOf(piece.position[1]);
   switch (piece.type) {
     case "rook":
       break;
@@ -18,6 +19,7 @@ const showLegalMoves = (piece: ChessPiece, alivePieces: ChessPiece[]) => {
     case "queen":
       break;
     case "king":
+      return kingLegalMoves(piece, alivePieces);
       break;
     case "pawn":
       break;
@@ -27,6 +29,7 @@ const showLegalMoves = (piece: ChessPiece, alivePieces: ChessPiece[]) => {
   }
 };
 
+// Check the position(must be <=8 and <= H)
 const isValidPosition = (columnIndex: number, rowIndex: number) => {
   return (
     letters.includes(letters[columnIndex]) &&
@@ -34,9 +37,12 @@ const isValidPosition = (columnIndex: number, rowIndex: number) => {
   );
 };
 
-const kingMoves = (position: string): string[] => {
-  const columnIdex = letters.indexOf(position[0]); // now column
-  const rowIndex = numbers.indexOf(position[1]); // now row
+const kingLegalMoves = (
+  king: ChessPiece,
+  alivePieces: ChessPiece[]
+): string[] => {
+  const columnIdex = letters.indexOf(king.position[0]); // now column
+  const rowIndex = numbers.indexOf(king.position[1]); // now row
 
   const directions = [
     // First number - column, second - row
@@ -51,27 +57,54 @@ const kingMoves = (position: string): string[] => {
     [1, 1],
   ]; // all squares around the king
 
-  const legalMoves: string[] = []; // array with all king moves
+  const probableMoves: string[] = []; // array with all king moves
 
   directions.forEach(([dx, dy]) => {
     const newColumnIndex = columnIdex + dx;
     const newRowIndex = rowIndex + dy;
 
     if (isValidPosition(newColumnIndex, newRowIndex)) {
-      // if new position legal, add it to array "legalMoves"
+      // create new position coordinates
       const newPosition = `${letters[newColumnIndex]}${numbers[newRowIndex]}`;
-      legalMoves.push(newPosition);
+
+      const pieceOnNewPosition = alivePieces.find(
+        (piece) => piece.position === newPosition
+      );
+
+      if (!pieceOnNewPosition) {
+        // no pieces on new position
+        probableMoves.push(newPosition);
+      } else if (pieceOnNewPosition.color !== king.color) {
+        // king can capture the figure (will delete this move
+        // later if it's under attack; check <<getAttackedSquares>> function
+        // in the code below)
+        probableMoves.push(newPosition);
+      }
     }
   });
+  // get all squares that are under attack
+  const squaresUnderAttack = getAttackedSquares(alivePieces, king.color);
+
+  // delete attacked squares from probableMoves
+  const legalMoves = probableMoves.filter(
+    (move) => !squaresUnderAttack.includes(move)
+  );
 
   return legalMoves;
 };
 
 const getAttackedSquares = (
   alivePieces: ChessPiece[],
-  color: "black" | "white"
+  color: "black" | "white" // color of the player, whose turn
 ): string[] => {
-  const getAttackedSquares: string[] = [];
+  const attackedSquares: string[] = [];
 
-  alivePieces.filter((piece) => piece.color !== color).forEach((piece) => {});
+  alivePieces
+    .filter((piece) => piece.color !== color)
+    .forEach((piece) => {
+      const pieceMoves = getLegalMoves(piece, alivePieces);
+
+      attackedSquares.push(...pieceMoves);
+    });
+  return attackedSquares;
 };
